5828bb42c8d915f0e7ca6b1b2c9bbc23605f3c9b
Jeralber <yeralvervargas@gmail.com>
2025-05-05 11:35:54 -0500

    Hasta el momento se creo una base de datos que no se habia creado, se realizo la creacion de products y se instalaron las dependencias de validator y transformer de los pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME || 'TestDB',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //Sincreoniza los cambio, funciona en desarrollo, pero no en produccion
+    }),
+    ProductsModule,
+  ],
 })
 export class AppModule {}
